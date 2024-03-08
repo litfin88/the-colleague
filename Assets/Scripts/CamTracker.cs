518c6aa27f1ec6935e4be6696c352243f064d32a
Emirhan <litfinemirhan@gmail.com>
2024-03-09 01:55:27 +0300

    Kamera script

---
+++ Assets/Scripts/CamTracker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTracker : MonoBehaviour
+{
+    private GameObject pivot;
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pivot = GameObject.Find("CamPivot");
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        pivot.transform.position = Vector3.Lerp(pivot.transform.position, player.transform.position, 0.03f);
+        this.gameObject.transform.LookAt(pivot.transform);
+    }
+}
